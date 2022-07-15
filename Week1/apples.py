cff833263f4440bb041359c07495c97cfd0ddc81
Dylan <BooshBear03@gmail.com>
2022-07-15 16:49:04 -0700

    Week1

---
+++ Week1/apples.py
@@ -0,0 +1,6 @@
+applePrice = 0.50
+name = input("Whats your name? ")
+print("Hi "+ name)
+apples = input("Apples cost $"+str(applePrice)+" How much would you like to buy? ")
+int(apples)
+print("Thank you "+name+"for buying "+str(apples)+" apples that cost $"+str(applePrice)+" each.")
